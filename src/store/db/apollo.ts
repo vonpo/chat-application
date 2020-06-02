02528ed31574af6409c808df335d29891035d1a9
Marcin Krawczyk <krawczykmarcin1987@gmail.com>
2020-06-02 22:11:55 +0200

    Save-dev work apollo.

--- src/store/db/apollo.ts
+++ src/store/db/apollo.ts
@@ -4,11 +4,34 @@ import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
 import { ChatMessage, ChatMessageInput } from "../chat/ChatMessage";
 import { IChatActions } from "./chatActions";
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
+const httpLink = new HttpLink({
   uri: "http://localhost:4000/",
 });
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
@@ -60,7 +83,28 @@ const addMessage = async (chatMessage: ChatMessageInput) => {
   return response.data?.message;
 };
 
+const subscribe = (onMessageReceived: (chatMessage: ChatMessage) => {}) => {
+  const observable = client.subscribe<{ addedMessage: ChatMessage }>({
+    query: gql`
+      subscription {
+        addedMessage {
+          text
+          id
+          userId
+        }
+      }
+    `,
+  });
+  console.info("observing");
+  observable.subscribe({
+    next(value) {
+      console.info(value, "xxxxxxxxxxxxx");
+    },
+  });
+};
+
 export const ChatActions: IChatActions = {
+  subscribe,
   getMessages,
   addMessage,
 };
